306e501503bf67cd39f600decf4bf2a417589f01
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-14 21:45:45 +0300

    add getallemployees request

---
+++ src/main/java/com/yehya/employeemanager/controllers/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.yehya.employeemanager.controllers;
+
+import com.yehya.employeemanager.model.Employee;
+import com.yehya.employeemanager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees () {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+   
+}
