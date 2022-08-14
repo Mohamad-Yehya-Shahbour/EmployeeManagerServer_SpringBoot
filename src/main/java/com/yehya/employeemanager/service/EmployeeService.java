84bf353cc0371ca778667599b3d1c79ea15978c3
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-14 21:38:28 +0300

    add employee services class

---
+++ src/main/java/com/yehya/employeemanager/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.yehya.employeemanager.service;
+
+import com.yehya.employeemanager.exception.UserNotFoundException;
+import com.yehya.employeemanager.model.Employee;
+import com.yehya.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    
+
+
+}
